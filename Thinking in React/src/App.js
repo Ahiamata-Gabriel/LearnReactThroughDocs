554d88b6121398b78455a529a25c4c0c08ef6a66
Ahiamata-Gabriel <ahiamatamawuli@gmail.com>
2023-06-30 11:19:19 +0000

    Adding Interactivity

---
+++ Thinking in React/src/App.js
@@ -0,0 +1,11 @@
+import './App.css';
+import Toolbar from './Components/Toolbar';
+
+export default function App() {
+  return (
+    <Toolbar
+      onPlayMovie={() => alert('Playing!')}
+      onUploadImage={() => alert('Uploading!')}
+    />
+  );
+}
