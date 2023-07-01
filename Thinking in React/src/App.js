29aabaed6f1ecce26355e6fc002ca45097334066
Ahiamata-Gabriel <ahiamatamawuli@gmail.com>
2023-07-01 15:38:05 +0000

    Quing

--- Thinking in React/src/App.js
+++ Thinking in React/src/App.js
@@ -1,11 +1,25 @@
-import './App.css';
-import Toolbar from './Components/Toolbar';
+import { useState } from 'react';
 
-export default function App() {
+export default function Counter() {
+  const [score, setScore] = useState(0);
+
+  function increment() {
+    setScore(score + 1);
+  }
+
   return (
-    <Toolbar
-      onPlayMovie={() => alert('Playing!')}
-      onUploadImage={() => alert('Uploading!')}
-    />
+    <>
+      <button onClick={() => increment()}>+1</button>
+      <button
+        onClick={() => {
+          increment();
+          increment();
+          increment();
+        }}
+      >
+        +3
+      </button>
+      <h1>Score: {score}</h1>
+    </>
   );
 }
