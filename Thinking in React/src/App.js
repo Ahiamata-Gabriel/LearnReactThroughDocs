bca8e22e9d28a119e8e70c14dae922d556621fc3
Ahiamata-Gabriel <ahiamatamawuli@gmail.com>
2023-07-01 15:40:35 +0000

    Quing

--- Thinking in React/src/App.js
+++ Thinking in React/src/App.js
@@ -3,18 +3,18 @@ import { useState } from 'react';
 export default function Counter() {
   const [score, setScore] = useState(0);
 
-  function increment() {
-    setScore(score + 1);
+  function increment(x) {
+    setScore(score + x);
   }
 
   return (
     <>
-      <button onClick={() => increment()}>+1</button>
+      <button onClick={() => increment(1)}>+1</button>
       <button
         onClick={() => {
-          increment();
-          increment();
-          increment();
+          increment(3);
+          // increment();
+          // increment();
         }}
       >
         +3
